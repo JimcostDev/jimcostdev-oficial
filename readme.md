c156da5acaab47f78711fcc656c15d5db197a68b
JimcostDev <rjak727@gmail.com>
2024-05-20 15:27:50 +0200

    migración a tailwindcss, se cambio todo el diseño

---
+++ readme.md
@@ -0,0 +1,10 @@
+## compilar 
+
+npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
+npm run tw:build
+
+## instalar plugins
+npm install -D @tailwindcss/forms
+npm install -D @tailwindcss/typography
+npm install -D @tailwindcss/aspect-ratio
+
---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        BACKGROUND_DESKTOP: "url('img/BACKGROUND_DESKTOP.webp')",
+        BACKGROUND_MOBILE: "url('img/BACKGROUND_MOBILE.webp')",
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#03A9F4",
+        secondary: "#F2FBFF",
+        tertiary: "#00142E",
+      }),
+      textColor: {
+        primary: "#03A9F4",
+        secondary: "#F2FBFF",
+        tertiary: "#00142E",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+      typography: {
+        h2: {
+          color: 'var(--text-primary)', 
+          fontSize: '1.25rem', 
+          fontWeight: 'normal', 
+          textAlign: 'center',
+          paddingTop: '0.5rem', 
+          paddingBottom: '1.25rem', 
+          textTransform: 'uppercase',
+        },
+      },
+      
+    },
+  },
+  variants: {
+    width: ["responsive", "hover", "focus"],
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+};
